d2a35ce81c9cef12343ddb54fbb54265348f62a1
unitcodedev <34649161+unitcodedev@users.noreply.github.com>
2017-12-21 16:26:49 +0700

    Create M_kendaraan.php

---
+++ app/models/M_kendaraan.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class M_kendaraan extends CI_Model {
+
+    public function insert($data) {
+		$sql = "INSERT INTO kendaraan(Kode, Keterangan)
+		VALUES('" .$data['kode'] ."', '" .$data['keterangan'] ."')";
+		$this->db->query($sql);
+		return $this->db->affected_rows();
+	}
+
+    public function update($data) {
+		$sql = "UPDATE kendaraan SET Kode='" .$data['kode'] ."',
+        Keterangan= '" .$data['keterangan'] ."'
+        WHERE ID='" .$data['id'] ."'";
+		$this->db->query($sql);
+		return $this->db->affected_rows();
+	}
+    
+    public function delete($data) {
+        $sql = "DELETE FROM kendaraan WHERE ID='" .$data['id'] ."'";
+		$this->db->query($sql);
+		return $this->db->affected_rows();
+        
+    }
+    
+}
