d8b69c67046b02332009ea092608c67927a90893
unitcodedev <34649161+unitcodedev@users.noreply.github.com>
2017-12-21 16:08:07 +0700

    Update Brand.php

--- app/controllers/Brand.php
+++ app/controllers/Brand.php
@@ -8,10 +8,11 @@ class Brand extends Auth_Controller {
     
      function __construct() {
         parent::__construct();
+         $this->load->model('M_brand');
     }
 
     public function listBrand() {
-        $result = $this->query->get_query_grid("SELECT * FROM jenisusaha");
+        $result = $this->query->get_query_grid("SELECT * FROM merk");
         if ($result != NULL) {
             echo json_encode(array('success' => 'true', 'data' => $result, 'title' => 'Info', 'msg' => 'List All Cabang'));
         } else {
@@ -20,37 +21,35 @@ class Brand extends Auth_Controller {
     }
 
     public function deleteBrand() {
-//        $this->db->trans_start();
-//        $this->db->delete('jenisusaha', array('ID' => $this->input->post('id')));
-//        $this->db->trans_complete();
-//        if (!$this->db->trans_status()) {
-//            echo json_encode(array('success' => 'false', 'data' => NULL, 'title' => 'Info', 'msg' => $this->db->error()));
-//        } else {
-//            echo json_encode(array('success' => 'true', 'data' => NULL, 'title' => 'Info', 'msg' => 'Proses Hapus Berhasil'));
-//        }
+        $this->db->trans_start();
+        $data = $this->input->post();
+        $this->M_brand->delete($data);
+        $this->db->trans_complete();
+        if (!$this->db->trans_status()) {
+            echo json_encode(array('success' => 'false', 'data' => NULL, 'title' => 'Info', 'msg' => $this->db->error()));
+        } else {
+            echo json_encode(array('success' => 'true', 'data' => NULL, 'title' => 'Info', 'msg' => 'Proses Hapus Berhasil'));
+        }
     }
 
     public function saveBrand() {
-//        $this->db->trans_start();
-//        $arr['Kode'] = $this->input->post('kode');
-//        $arr['Keterangan'] = $this->input->post('keterangan');
-//        if ($this->input->post('id') == '0') {
-//            // Insert
-//            $this->db->insert('jenisusaha', $arr);
-//            $msg = "Proses Simpan Berhasil";
-//        } else {
-//            // Update
-//            $this->db->update('jenisusaha', $arr, array('ID' => $this->input->post('id')));
+        $this->db->trans_start();
+        $data = $this->input->post();
+        if ($this->input->post('id') == '0') {
+            $this->M_brand->insert($data);
+            $msg = "Proses Simpan Berhasil";
+        } else {
+            // Update
+            $this->M_brand->update($data);
 //            log_message('error', $this->db->last_query());
-//            $msg = "Proses Update Berhasil";
-//        }
-//        $this->db->trans_complete();
-//        if (!$this->db->trans_status()) {
-//            echo json_encode(array('success' => 'false', 'data' => NULL, 'title' => 'Info', 'msg' => $this->db->error()));
-//        } else {
-//            echo json_encode(array('success' => 'true', 'data' => NULL, 'title' => 'Info', 'msg' => $msg));
-//        }
+            $msg = "Proses Update Berhasil";
+        }
+        $this->db->trans_complete();
+        if (!$this->db->trans_status()) {
+            echo json_encode(array('success' => 'false', 'data' => NULL, 'title' => 'Info', 'msg' => $this->db->error()));
+        } else {
+            echo json_encode(array('success' => 'true', 'data' => NULL, 'title' => 'Info', 'msg' => $msg));
+        }
     }
-
     
 }
