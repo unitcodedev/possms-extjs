58a0aa20df7d26f8bcad11eb856e078ee3224224
unitcodedev <34649161+unitcodedev@users.noreply.github.com>
2017-12-21 16:09:45 +0700

    Update M_brand.php

--- app/models/M_brand.php
+++ app/models/M_brand.php
@@ -3,6 +3,28 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Brand extends CI_Model {
+class M_brand extends CI_Model {
 
+    public function insert($data) {
+		$sql = "INSERT INTO merk(Kode, Keterangan)
+		VALUES('" .$data['kode'] ."', '" .$data['keterangan'] ."')";
+		$this->db->query($sql);
+		return $this->db->affected_rows();
+	}
+
+    public function update($data) {
+		$sql = "UPDATE merk SET Kode='" .$data['kode'] ."',
+        Keterangan= '" .$data['keterangan'] ."'
+        WHERE ID='" .$data['id'] ."'";
+		$this->db->query($sql);
+		return $this->db->affected_rows();
+	}
+    
+    public function delete($data) {
+        $sql = "DELETE FROM merk WHERE ID='" .$data['id'] ."'";
+		$this->db->query($sql);
+		return $this->db->affected_rows();
+        
+    }
+    
 }
